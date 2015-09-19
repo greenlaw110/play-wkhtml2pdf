e16f0ae649633e1dba19a4b64706feba580de38a
Gelin Luo <greenlaw110@gmail.com>
2015-09-20 07:23:44 +1000

    archive project

---
+++ src/play/modules/wkhtml2pdf/RenderPDF.java
@@ -0,0 +1,15 @@
+package play.modules.wkhtml2pdf;
+
+import play.mvc.Http.Request;
+import play.mvc.Http.Response;
+import play.mvc.results.Result;
+
+public class RenderPDF extends Result {
+    
+    @Override
+    public void apply(Request request, Response response) {
+	// TODO Auto-generated method stub
+
+    }
+
+}
